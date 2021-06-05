8ab9fc546a24037b1d06064763e8d7baf6720b36
Aliasgar Merchant <aliasgarm2000@gmail.com>
2021-06-05 23:06:13 +0530

    Make changes to deployment test for node

---
+++ build.sh
@@ -0,0 +1,17 @@
+sudo rosdep init
+
+rosdep update
+
+echo export ROS_DISTRO='melodic' >> ~/.bashrc
+
+echo export ROS_PYTHON_VERSION='2' >> ~/.bashrc
+
+catkin build
+
+source environment.sh
+
+source ./devel/setup.bash
+
+source ./aliases
+
+echo source ~/home/Desktop/Deployment-Test/devel/setup.bash >> ~/.bashrc
---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+sudo apt install python-catkin-tools
+
+sudo apt install python-pip
+
+sudo apt upgrade libignition-math2
+
+pip install shapely
+
+pip install wstool
+
+pip install setuptools
+
+sudo apt install checkinstall
+
+sudo apt install git-lfs
+
+cd ~
+
+git 
+
+cd ~/ROS-System
+
+catkin init
+
