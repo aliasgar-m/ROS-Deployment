b77ba73c2d8a38a3bbb89551039e23306016f1c0
Aliasgar Merchant <aliasgarm2000@gmail.com>
2021-06-09 21:18:10 +0530

    final update to setup

--- setup.sh
+++ setup.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-sudo apt install -y \
-    python-rosdep \
-    python-catkin-tools \
-    python-pip \
-    rosinstall \
-    rosinstall-generator \
-    checkinstall \
-    git-lfs
+sudo apt install python-rosdep python-catkin-tools python-pip -y
+
+sudo apt install rosinstall rosinstall-generator checkinstall git-lfs -y
 
 sudo python -m pip install --upgrade pip
 
-pip install -y \
-    shapely \
-    wstool \
-    setuptools
+pip install shapely wstool setuptools
 
-sudo apt upgrade libignition-math2
+sudo apt upgrade libignition-math2 -y
