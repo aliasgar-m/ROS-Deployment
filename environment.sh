0f7792d38326ec2684f295fc26d6f15e6223b3b1
Aliasgar Merchant <aliasgarm2000@gmail.com>
2021-06-11 13:28:22 +0530

    revert to org environment.sh

--- environment.sh
+++ environment.sh
@@ -2,16 +2,18 @@
 
 CATKIN_SHELL=bash
 
-export DEPLOY_ROOT=$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)
+export FSD_ROOT=$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)
+
+printf "Sourcing Deployment-Test/devel/setup.bash\n"
 
 # check whether devel folder exists
-if [ -f "${DEPLOY_ROOT}/devel/setup.bash" ]; then
+if [ -f "${FSD_ROOT}/devel/setup.bash" ]; then
     # source setup.sh from same directory as this file
-    source "${DEPLOY_ROOT}/devel/setup.bash"
+    source "${FSD_ROOT}/devel/setup.bash"
 else
     source "/opt/ros/melodic/setup.bash"
     printf "You need to build first before you can source\n"
     printf "Run 'catkin build' in the skeleton_repo directory\n"
 fi
 
-source ${DEPLOY_ROOT}/aliases
+source ${FSD_ROOT}/fsd_aliases
