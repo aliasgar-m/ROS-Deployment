7d9b433c7dcccc9c4c85f547626ead8d932a8d9e
Aliasgar Merchant <aliasgarm2000@gmail.com>
2021-06-13 17:23:29 +0530

    removed source added printf

--- environment.sh
+++ environment.sh
@@ -3,17 +3,18 @@
 CATKIN_SHELL=bash
 
 export FSD_ROOT= "/home/Desktop/Deployment-Test"
+printf FSD_ROOT
 
-printf "Sourcing Deployment-Test/devel/setup.bash\n"
+# printf "Sourcing Deployment-Test/devel/setup.bash\n"
 
-# check whether devel folder exists
-if [ -f "FSD_ROOT/devel/setup.bash" ]; then
-    # source setup.sh from same directory as this file
-    source "FSD_ROOT/devel/setup.bash"
-else
-    source "/opt/ros/melodic/setup.bash"
-    printf "You need to build first before you can source\n"
-    printf "Run 'catkin build' in the skeleton_repo directory\n"
-fi
+# # check whether devel folder exists
+# if [ -f "FSD_ROOT/devel/setup.bash" ]; then
+#     # source setup.sh from same directory as this file
+#     source "FSD_ROOT/devel/setup.bash"
+# else
+#     source "/opt/ros/melodic/setup.bash"
+#     printf "You need to build first before you can source\n"
+#     printf "Run 'catkin build' in the skeleton_repo directory\n"
+# fi
 
 # source ${FSD_ROOT}/aliases
