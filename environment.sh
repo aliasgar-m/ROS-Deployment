3247df5729f85594724255dbf8876891bced8615
Aliasgar Merchant <aliasgarm2000@gmail.com>
2021-06-13 14:30:35 +0530

    changed path for FSD_ROOT

--- environment.sh
+++ environment.sh
@@ -2,14 +2,14 @@
 
 CATKIN_SHELL=bash
 
-export FSD_ROOT= pwd
+export FSD_ROOT= "/home/Desktop/Deployment-Test"
 
 printf "Sourcing Deployment-Test/devel/setup.bash\n"
 
 # check whether devel folder exists
 if [ -f "${FSD_ROOT}/devel/setup.bash" ]; then
     # source setup.sh from same directory as this file
-    source "FSD_ROOT/devel/setup.bash"
+    source "${FSD_ROOT}/devel/setup.bash"
 else
     source "/opt/ros/melodic/setup.bash"
     printf "You need to build first before you can source\n"
