3f246723619f4c3a9953c3a9d8f8c413603f259e
Aliasgar Merchant <aliasgarm2000@gmail.com>
2021-06-13 17:44:52 +0530

    added if condition for source

--- environment.sh
+++ environment.sh
@@ -4,13 +4,13 @@ CATKIN_SHELL=bash
 
 export FSD_ROOT=$( pwd ) 
 
-printf "ROS Deployment Test 1"
+printf "ROS Deployment Test 1\n"
 printf "Sourcing Deployment-Test/devel/setup.bash\n"
 
-# # check whether devel folder exists
-# if [ -f "FSD_ROOT/devel/setup.bash" ]; then
-#     # source setup.sh from same directory as this file
-#     source "FSD_ROOT/devel/setup.bash"
+# check whether devel folder exists
+if [ -f "${FSD_ROOT}/devel/setup.bash" ]; then
+    # source setup.sh from same directory as this file
+    source "FSD_ROOT/devel/setup.bash"
 # else
 #     source "/opt/ros/melodic/setup.bash"
 #     printf "You need to build first before you can source\n"
