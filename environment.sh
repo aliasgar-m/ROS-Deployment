76afb53dc8cc49eb7fe01a097843a998128edf56
Aliasgar Merchant <aliasgarm2000@gmail.com>
2021-06-13 17:51:19 +0530

    added else condition printf

--- environment.sh
+++ environment.sh
@@ -10,8 +10,9 @@ printf "Sourcing Deployment-Test/devel/setup.bash\n"
 # check whether devel folder exists
 if [ -f "${FSD_ROOT}/devel/setup.bash" ]; then
     # source setup.sh from same directory as this file
-    source "FSD_ROOT/devel/setup.bash"
-# else
+    source "${FSD_ROOT}/devel/setup.bash"
+else
+    printf "No File Found \n"
 #     source "/opt/ros/melodic/setup.bash"
 #     printf "You need to build first before you can source\n"
 #     printf "Run 'catkin build' in the skeleton_repo directory\n"
