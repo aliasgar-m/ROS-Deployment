fc034895bee3035c3762aa80b62a13b7d681690c
Aliasgar Merchant <aliasgarm2000@gmail.com>
2021-06-13 19:30:27 +0530

    removed sudo keyword

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo apt-get install python-rosdep python-catkin-tools python-pip -y
+apt-get install python-rosdep python-catkin-tools python-pip -y
 
-sudo apt-get install python-rosinstall python-rosinstall-generator checkinstall git-lfs -y
+apt-get install python-rosinstall python-rosinstall-generator checkinstall git-lfs -y
 
-sudo python -m pip install --upgrade pip
+python -m pip install --upgrade pip
 
 pip install shapely wstool setuptools
 
-sudo apt-get upgrade libignition-math2 -y
+apt-get upgrade libignition-math2 -y
