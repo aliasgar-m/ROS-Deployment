68290eb9bc5d540999083b5d20e3537be474a0fa
Aliasgar Merchant <aliasgarm2000@gmail.com>
2021-06-13 17:16:01 +0530

    changed source path

--- environment.sh
+++ environment.sh
@@ -9,11 +9,11 @@ printf "Sourcing Deployment-Test/devel/setup.bash\n"
 # check whether devel folder exists
 if [ -f "FSD_ROOT/devel/setup.bash" ]; then
     # source setup.sh from same directory as this file
-    source FSD_ROOT/devel/setup.bash
+    source "FSD_ROOT/devel/setup.bash"
 else
     source "/opt/ros/melodic/setup.bash"
     printf "You need to build first before you can source\n"
     printf "Run 'catkin build' in the skeleton_repo directory\n"
 fi
 
-source ${FSD_ROOT}/aliases
+# source ${FSD_ROOT}/aliases
